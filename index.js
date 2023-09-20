188ef051987115bc1f404862960094eccfa415cf
harneeetah <harneeetah@gmail.com>
2023-09-20 14:46:27 +0100

    eventlog

---
+++ index.js
@@ -0,0 +1,15 @@
+const logEvents = require('./logEvents');
+const EventEmitter = require('events');
+
+class MyEmitter extends EventEmitter {};
+
+//initialize object
+const myEmitter = new MyEmitter()
+
+//adding a listener for the log event
+myEmitter.on('log',(msg) => logEvents(msg));
+
+setTimeout(() => {
+    //Emit event
+    myEmitter.emit('log', 'log event emmitted!');
+}, 2000);
